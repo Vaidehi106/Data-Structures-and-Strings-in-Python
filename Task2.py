05f4d666055684b39aeba265f5cdd45d2f81e1d8
Vaidehi106 <patelvaidehi1006@gmail.com>
2025-04-04 23:07:06 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,9 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+print('Original list: ' + str(list))
+
+slicing=list[0:5:1]
+print('Extracted first five elements: ' + str(slicing))
+
+reversing=slicing[::-1]
+print('Reversed first five elements: ' + str(reversing))
+
